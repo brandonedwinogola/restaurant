7770e40f75864d9d96d0112d138c597ccc991a46
brandon <brandon14ogola@gmail.com>
2023-02-23 17:07:14 +0300

    updates

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -13,7 +13,7 @@ const AboutUs = () => (
       <div className="app__aboutus-content_about">
         <h1 className="headtext__cormorant">About Us</h1>
         <img src={images.spoon} alt="about_spoon" className="spoon__img" />
-        <p className="p__opensans">Welcome to Gericht, an Italian restaurant located in the heart of downtown nairobi.At La Gericht we're passionate about creating delicious, authentic Italian cuisine that showcases the flavors and ingredients of our homeland. Our menu features classic dishes like spaghetti carbonara, veal parmigiana, and tiramisu, as well as seasonal specials and vegetarian options.
+        <p className="p__opensans">Welcome to Klabu, a Kenyan restaurant located in the heart of University of Nairobi.At Klabu we're passionate about creating delicious, authentic Kenyan cuisine that showcases the flavors and ingredients of our homeland. Our menu features classic dishes like spaghetti carbonara, veal parmigiana, and tiramisu, as well as seasonal specials and vegetarian options.
 We believe in using only the freshest, highest quality ingredients, and we work closely with local farmers and producers to source our ingredients whenever possible. We also take pride in our commitment to sustainability and minimizing our environmental impact.
 In addition to our delicious food, we're also dedicated to providing excellent service and creating a welcoming atmosphere for all of our guests. Whether you're here for a romantic date night, a family dinner, or a business lunch, we want you to feel like you're part of our family.Thank you for choosing La Vita, and we look forward to serving you soon.</p>
         <button type="button" className="custom__button">Know More</button>
@@ -26,7 +26,7 @@ In addition to our delicious food, we're also dedicated to providing excellent s
       <div className="app__aboutus-content_history">
         <h1 className="headtext__cormorant">Our History</h1>
         <img src={images.spoon} alt="about_spoon" className="spoon__img" />
-        <p className="p__opensans">Our restaurant was founded in 2023 by chef Brandon Ogola, who grew up in a small village in Kisumu and has been cooking traditional Italian dishes for over 20 years.</p>
+        <p className="p__opensans">Our restaurant was founded in 2023 by chef Brandon Ogola, who grew up in a small village in Kisumu and has been cooking traditional Kenyan dishes for over 20 years.</p>
         <button type="button" className="custom__button">Know More</button>
       </div>
     </div>
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => (
     <div className="app__wrapper_info">
       <SubHeading title="Chase the new flavour" />
       <h1 className="app__header-h1">The Key To Fine Dining</h1>
-      <p className="p__opensans" style={{ margin: '2rem 0' }}>Welcome to Gericht - Where every meal is a masterpiece. From the moment you step through our doors, you'll be transported to a world of culinary delights, where every dish is crafted with care and passion. Whether you're joining us for a romantic dinner for two or a lively celebration with friends, our team is dedicated to creating an unforgettable dining experience that will leave you wanting more. Come dine with us and discover why Gericht is more than just a restaurant - It's a journey of the senses. </p>
+      <p className="p__opensans" style={{ margin: '2rem 0' }}>Welcome to Klabu - Where every meal is a masterpiece. From the moment you step through our doors, you'll be transported to a world of culinary delights, where every dish is crafted with care and passion. Whether you're joining us for a romantic dinner for two or a lively celebration with friends, our team is dedicated to creating an unforgettable dining experience that will leave you wanting more. Come dine with us and discover why Gericht is more than just a restaurant - It's a journey of the senses. </p>
       <button type="button" className="custom__button">Explore Menu</button>
     </div>
 
